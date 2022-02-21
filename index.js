915967d6475bab7f08670ecaa4afbdeed11f9d2d
ArukaBay <100147888+ArukaBay@users.noreply.github.com>
2022-02-21 20:53:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(".white").click(function() {
+        $("#gradient").css("background-color","#dcdcdc"); //edit, body must be in quotes!
+        $("p").css("color", "#1c222a");
+        $("h4").css("color", "#1c222a");
+        $("#contact").css("border-top", "10px solid #1c222a");
+        $("#title").css("border-bottom", "10px solid #1c222a");
+    });
+});
+$(document).ready(function() {
+    $(".black").click(function() {
+        $("#gradient").css("background-color","#1c222a"); //edit, body must be in quotes!
+        $("p").css("color", "#dcdcdc");
+        $("h4").css("color", "#dcdcdc");
+        $("#contact").css("border-top", "10px solid #dcdcdc");
+        $("#title").css("border-bottom", "10px solid #dcdcdc");
+    });
+});
+
+$(document).ready(function(){
+      $("p").toggle();
+  });
+  $("h4").click(function(){
+    $("p").toggle(1000);
+  });
